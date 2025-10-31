5f3f3a2eddc96fed902944d1d2b304c3497d49c0
Uli Rodriguez <ulirodriguez@Ulis-MacBook-Pro.local>
2025-10-31 12:48:19 -0700

    removed unneeded files

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/ulirodriguez/CodeProjects/PH_Viz
-# Build directory: /Users/ulirodriguez/CodeProjects/PH_Viz/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(UtilsTests "/Users/ulirodriguez/CodeProjects/PH_Viz/build/test_utils")
-set_tests_properties(UtilsTests PROPERTIES  _BACKTRACE_TRIPLES "/Users/ulirodriguez/CodeProjects/PH_Viz/CMakeLists.txt;167;add_test;/Users/ulirodriguez/CodeProjects/PH_Viz/CMakeLists.txt;0;")
